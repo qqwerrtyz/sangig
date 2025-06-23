497406ee74a6147b2ebfc1df1588b417b5682d40
cruciffix <mr.unnona@gmail.com>
2025-06-23 08:08:24 +0300

    final commit XXXD

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 import os
-from app import app
+from app import app as application   # импортируем ваше Flask-приложение как application
 
+# Переменная application — это стандартная точка входа для WSGI-серверов.
+# Ниже блок на случай прямого запуска через python wsgi.py (не обязателен в проде)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    application.run(host='0.0.0.0', port=port)
